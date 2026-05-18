0a63362b40ad3a5801026cac33fa0d629d9fb86e
alarconvv <alarconvv@gmail.com>
2026-05-18 15:10:52 -0400

    Vs26.05.18.11510

--- SetUpPackage.R
+++ SetUpPackage.R
@@ -30,3 +30,35 @@ usethis::use_package_doc()
 usethis::use_readme_md()
 
 
+
+## Add packages
+usethis::use_package("shiny")
+usethis::use_package("bslib")
+usethis::use_package("ape")
+usethis::use_package("phytools")
+usethis::use_package("geiger")
+usethis::use_package("nlme")
+usethis::use_package("dplyr")
+usethis::use_package("readr")
+usethis::use_package("readxl")
+usethis::use_package("jsonlite")
+usethis::use_package("shinyAce")
+usethis::use_package("shiny.i18n")
+usethis::use_package("rlang")
+usethis::use_package("cli")
+usethis::use_package("checkmate")
+
+# Dependences for development
+usethis::use_package("testthat", type = "Suggests")
+usethis::use_package("devtools", type = "Suggests")
+usethis::use_package("roxygen2", type = "Suggests")
+
+# Lisence
+usethis::use_gpl_license(version = 3, include_future = TRUE)
+
+
+# I will keep two branches for this repo
+#-----------------------------------------
+
+# main = develop
+# master = stable version
